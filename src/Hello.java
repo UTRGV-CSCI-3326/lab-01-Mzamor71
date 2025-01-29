f385054cd92e6e1430bad98ba797be0a15687721
Mzamor71 <100431992+Mzamor71@users.noreply.github.com>
2025-01-29 01:28:33 +0000

    New Changes

--- src/Hello.java
+++ src/Hello.java
@@ -1,4 +1,11 @@
-// Hello World
+/*
+Name: Michael Zamora
+SID: 20593343
+Date: 1/28/2025
+
+Description: This program is a simple way of printing "Hello World!".
+
+*/
 public class Hello{
     public static void main(String[] args){
         System.out.println("Hello World!");
